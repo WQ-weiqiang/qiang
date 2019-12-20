0470c181f7d764d7ccea67e6ed6576be4115790c
WQ-weiqiang <WQ-weiqiang@users.noreply.github.com>
2019-12-20 17:50:22 +0800

    nlp

--- java-base/src/main/java/com/qiang/Test.java
+++ java-base/src/main/java/com/qiang/Test.java
@@ -1,8 +1,22 @@
 package com.qiang;
 
 import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.collection.AhoCorasick.AhoCorasickDoubleArrayTrie;
+import com.hankcs.hanlp.dictionary.CoreDictionary;
+import com.hankcs.hanlp.dictionary.CustomDictionary;
+import com.hankcs.hanlp.model.crf.CRFLexicalAnalyzer;
+import com.hankcs.hanlp.seg.Dijkstra.DijkstraSegment;
+import com.hankcs.hanlp.seg.NShort.NShortSegment;
+import com.hankcs.hanlp.seg.Segment;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.IndexTokenizer;
+import com.hankcs.hanlp.tokenizer.NLPTokenizer;
+import com.hankcs.hanlp.tokenizer.SpeedTokenizer;
+import com.hankcs.hanlp.tokenizer.StandardTokenizer;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -27,7 +41,7 @@ public class Test {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        int[] arr = generatorIntArray(100);
 //        System.out.println(arr.length+Arrays.toString(arr));
 //        selectSort(arr);
@@ -43,11 +57,81 @@ public class Test {
         Runtime rt = Runtime.getRuntime();
         System.out.println("Total Memory = " + rt.totalMemory() + " ,Free Memory= " + rt.freeMemory());
 
-//        System.out.println("abc" + " = " + 3.2F);
+        //        System.out.println("abc" + " = " + 3.2F);
 
         System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+        List<Term> termList = StandardTokenizer.segment("商品和服务");
+        System.out.println(termList);
+        System.out.println("----------------------------------------");
 
+        System.out.println(NLPTokenizer.segment("我新造一个词叫幻想乡你能识别并标注正确词性吗？"));
+        // 注意观察下面两个“希望”的词性、两个“晚霞”的词性
+        System.out.println(NLPTokenizer.analyze("我的希望是希望张晚霞的背影被晚霞映红").translateLabels());
+        System.out.println(NLPTokenizer.analyze("支援臺灣正體香港繁體：微软公司於1975年由比爾·蓋茲和保羅·艾倫創立。"));
+
+        System.out.println("----------------------------------------");
+        termList = IndexTokenizer.segment("主副食品");
+        for (Term term : termList) {
+            System.out.println(term + " [" + term.offset + ":" + (term.offset + term.word.length()) + "]");
+        }
+        System.out.println("----------------------------------------");
+
+        //N 最短路径分词
+        Segment nShortSegment = new NShortSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        Segment shortestSegment = new DijkstraSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        String[] testCase = new String[]{
+                "今天，刘志军案的关键人物,山西女商人丁书苗在市二中院出庭受审。",
+                "刘喜杰石国祥会见吴亚琴先进事迹报告团成员",
+        };
+        for (String sentence : testCase) {
+            System.out.println("N-最短分词：" + nShortSegment.seg(sentence) + "\n最短路分词：" + shortestSegment.seg(sentence));
+        }
+
+
+        System.out.println("-------------------CRF分词---------------------");
+        CRFLexicalAnalyzer analyzer = new CRFLexicalAnalyzer();
+        String[] tests = new String[]{
+                "商品和服务",
+                "上海华安工业（集团）公司董事长谭旭光和秘书胡花蕊来到美国纽约现代艺术博物馆参观",
+                "微软公司於1975年由比爾·蓋茲和保羅·艾倫創立，18年啟動以智慧雲端、前端為導向的大改組。" // 支持繁体中文
+        };
+        for (String sentence : tests) {
+            System.out.println(analyzer.analyze(sentence));
+        }
+
+        System.out.println("-------------------极速词典分词---------------------");
+        String text = "江西鄱阳湖干枯，中国最大淡水湖变成大草原";
+        System.out.println(SpeedTokenizer.segment(text));
+        long start = System.currentTimeMillis();
+        int pressure = 1000000;
+        for (int i = 0; i < pressure; ++i) {
+            SpeedTokenizer.segment(text);
+        }
+        double costTime = (System.currentTimeMillis() - start) / (double) 1000;
+        System.out.printf("分词速度：%.2f字每秒", text.length() * pressure / costTime);
+
+        System.out.println("-------------------自定义词典分词---------------------");
+        // 动态增加
+        CustomDictionary.add("攻城狮");
+        // 强行插入
+        CustomDictionary.insert("白富美", "nz 1024");
+        // 删除词语（注释掉试试）
+//        CustomDictionary.remove("攻城狮");
+        System.out.println(CustomDictionary.add("单身狗", "nz 1024 n 1"));
+        System.out.println(CustomDictionary.get("单身狗"));
+
+        text = "攻城狮逆袭单身狗，迎娶白富美，走上人生巅峰";  // 怎么可能噗哈哈！
+
+        // AhoCorasickDoubleArrayTrie自动机扫描文本中出现的自定义词语
+        final char[] charArray = text.toCharArray();
+        CustomDictionary.parseText(charArray, new AhoCorasickDoubleArrayTrie.IHit<CoreDictionary.Attribute>() {
+            @Override
+            public void hit(int begin, int end, CoreDictionary.Attribute value) {
+                System.out.printf("[%d:%d]=%s %s\n", begin, end, new String(charArray, begin, end - begin), value);
+            }
+        });
+
+        // 自定义词典在所有分词器中都有效
+        System.out.println(HanLP.segment(text));
     }
-
-
 }
